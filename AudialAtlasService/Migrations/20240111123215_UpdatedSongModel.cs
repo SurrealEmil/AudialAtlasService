8f0aa5dd2e011db21ff97a54a50fad3627c7bc96
Balos87 <145317796+Balos87@users.noreply.github.com>
2024-01-11 13:34:16 +0100

    Updated Song
    Removed field

---
+++ AudialAtlasService/Migrations/20240111123215_UpdatedSongModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AudialAtlasService.Migrations
+{
+    public partial class UpdatedSongModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Artists_ArtistsArtistId",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Artist",
+                table: "Songs");
+
+            migrationBuilder.RenameColumn(
+                name: "ArtistsArtistId",
+                table: "Songs",
+                newName: "ArtistId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Songs_ArtistsArtistId",
+                table: "Songs",
+                newName: "IX_Songs_ArtistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Artists_ArtistId",
+                table: "Songs",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "ArtistId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Artists_ArtistId",
+                table: "Songs");
+
+            migrationBuilder.RenameColumn(
+                name: "ArtistId",
+                table: "Songs",
+                newName: "ArtistsArtistId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Songs_ArtistId",
+                table: "Songs",
+                newName: "IX_Songs_ArtistsArtistId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Artist",
+                table: "Songs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Artists_ArtistsArtistId",
+                table: "Songs",
+                column: "ArtistsArtistId",
+                principalTable: "Artists",
+                principalColumn: "ArtistId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- AudialAtlasService/Models/Song.cs
+++ AudialAtlasService/Models/Song.cs
@@ -4,11 +4,8 @@
     {
         public int SongId { get; set; }
         public string SongTitle { get; set; }
-        public string Artist { get; set; }
-
+        public virtual Artist Artist { get; set; }
         public virtual ICollection<User> Users { get; set; }
         public virtual ICollection<Genre> Genres { get; set; }
-
-        public virtual Artist Artists { get; set; }
     }
 }
