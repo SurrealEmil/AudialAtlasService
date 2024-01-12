8865bc85cbba786e6ba4d11b62f97385a30651a7
Balos87 <145317796+Balos87@users.noreply.github.com>
2024-01-12 05:56:08 +0100

    Create New Branch
    Created new branch to create DTO and view model

---
+++ AudialAtlasService/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using AudialAtlasService.Data;
+using AudialAtlasService.Models;
+
+namespace AudialAtlasService.DTOs
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
