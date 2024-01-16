1d23976e7735c6aedcff386862dd54cb05ae0b7e
SurrealEmil <emil.ejderklev1@hotmail.com>
2024-01-16 14:23:16 +0100

    Add Genre

---
+++ AudialAtlasService/Models/ViewModels/GenreViewModels/ArtistsInGenreViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AudialAtlasService.Models.ViewModels.GenreViewModels
+{
+    public class ArtistsInGenreViewModel : GenreSingleViewModel
+    {
+        public virtual List<ArtistSingleViewModel> Artists { get; set; }
+    }
+}
---
+++ AudialAtlasService/Models/ViewModels/GenreViewModels/GenreSingleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AudialAtlasService.Models.ViewModels.GenreViewModels
+{
+    public class GenreSingleViewModel
+    {
+        public int GenreId { get; set; }
+        public string GenreTitle { get; set; }
+    }
+}
