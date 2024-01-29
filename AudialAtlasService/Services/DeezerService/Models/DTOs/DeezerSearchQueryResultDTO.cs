f96db349b938e8b3f9f6a73e2cf4f3effc999f65
Pontus Ahlbäck <pontus.ahlback93@gmail.com>
2024-01-29 15:50:10 +0100

    Add DeezerServiceHandler

---
+++ AudialAtlasService/Services/DeezerService/Models/DTOs/DeezerSearchQueryResultDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AudialAtlasService.Services.DeezerService.Models.DTOs
+{
+    public class DeezerSearchResultDTO
+    {
+        [JsonPropertyName("data")]
+        public List<DeezerSearchResultData> Data { get; set; }
+
+        public class DeezerSearchResultData
+        {
+            [JsonPropertyName("artist")]
+            public DeezerSearchResultArtist DeezerArtistInfo { get; set; }
+        }
+
+        public class DeezerSearchResultArtist
+        {
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+        }
+    }
+
+    
+}
---
+++ AudialAtlasService/Services/DeezerService/Models/DTOs/DeezerTopFiveSongsDTO.cs
@@ -0,0 +1,19 @@
+﻿using static AudialAtlasService.Services.DeezerService.Models.DTOs.DeezerSearchResultDTO;
+using System.Text.Json.Serialization;
+
+namespace AudialAtlasService.Services.DeezerService.Models.DTOs
+{
+    public class DeezerTopFiveSongsDTO
+    {
+        [JsonPropertyName("data")]
+        public List<DeezerTopFiveSongsDtoTracks> Data { get; set; }
+
+        public class DeezerTopFiveSongsDtoTracks
+        {
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+            [JsonPropertyName("title")]
+            public string Title { get; set; }
+        }
+    }
+}
